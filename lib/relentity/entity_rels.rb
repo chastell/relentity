0278f2c6f785ec0e150a2c61968078ff2d9fc6c6
Piotr Szotkowski <chastell@chastell.net>
2011-01-20 20:20:42 +0100

    introducing EntityRels#method_missing

--- lib/relentity/entity_rels.rb
+++ lib/relentity/entity_rels.rb
@@ -1,2 +1,13 @@
 module Relentity class EntityRels
+
+  def initialize entity
+    @rels = Rels.select { |rel| rel.refs? entity }
+                .group_by { |rel| rel.rel_to_other entity }
+                .each_value { |rels| rels.map! { |rel| rel.other entity } }
+  end
+
+  def method_missing method, *args, &block
+    @rels[method]
+  end
+
 end end
--- spec/relentity/entity_rels_spec.rb
+++ spec/relentity/entity_rels_spec.rb
@@ -1,2 +1,24 @@
 module Relentity describe EntityRels do
+
+  describe '#method_missing' do
+
+    it 'retrieves Entities based on relationship' do
+      sam   = People.id :sam
+      sybil = People.id :sybil
+      y_sam = People.id :y_sam
+
+      sams   = EntityRels.new sam
+      sybils = EntityRels.new sybil
+      y_sams = EntityRels.new y_sam
+
+      sams.spouses.should    include sybil
+      sybils.spouses.should  include sam
+      sams.children.should   include y_sam
+      sybils.children.should include y_sam
+      y_sams.parents.should  include sam
+      y_sams.parents.should  include sybil
+    end
+
+  end
+
 end end
