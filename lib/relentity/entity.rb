298e0b8be3ac54035d318e93f8daecbceb56dab2
Piotr Szotkowski <chastell@chastell.net>
2011-01-20 07:58:23 +0100

    introducing the Entity magic

--- lib/relentity/entity.rb
+++ lib/relentity/entity.rb
@@ -1,2 +1,11 @@
 module Relentity module Entity
+
+  def initialize hash
+    @properties = hash
+  end
+
+  def method_missing method, *args, &block
+    @properties[method]
+  end
+
 end end
--- spec/relentity/entity_spec.rb
+++ spec/relentity/entity_spec.rb
@@ -1,2 +1,17 @@
 module Relentity describe Entity do
+
+  class Person
+    include Entity
+  end
+
+  describe '#method_missing' do
+
+    it 'retrieves arbitrary properties set upon initialisation' do
+      librarian = Person.new given_names: ['Horace'], surname: 'Worblehat'
+      librarian.given_names.should == ['Horace']
+      librarian.surname.should     == 'Worblehat'
+    end
+
+  end
+
 end end
