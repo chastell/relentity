53d86aa9a039297c631088ac0ba50d536ddb552e
Piotr Szotkowski <chastell@chastell.net>
2011-01-20 19:04:51 +0100

    introducing Rel#refs?

--- lib/relentity/rel.rb
+++ lib/relentity/rel.rb
@@ -1,2 +1,9 @@
 module Relentity class Rel
+
+  include Entity
+
+  def refs? entity
+    refs.include? entity
+  end
+
 end end
--- spec/relentity/rel_spec.rb
+++ spec/relentity/rel_spec.rb
@@ -1,2 +1,17 @@
 module Relentity describe Rel do
+
+  describe '#refs?' do
+
+    it 'is a predicate whether the Rel refs the given Entity' do
+      mort    = Person.new id: :mort
+      ysabell = Person.new id: :ysabell
+      susan   = Person.new id: :susan
+      duchy   = Rel.new refs: [mort, ysabell]
+      duchy.should     be_refs mort
+      duchy.should     be_refs ysabell
+      duchy.should_not be_refs susan
+    end
+
+  end
+
 end end
