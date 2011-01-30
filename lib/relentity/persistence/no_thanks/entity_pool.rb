9a399e01d4e074e6c2f77683eb42e9bf3342381a
Piotr Szotkowski <chastell@chastell.net>
2011-01-30 14:07:23 +0100

    fill-in Persistence::NoThanks::EntityPool

--- lib/relentity/persistence/no_thanks/entity_pool.rb
+++ lib/relentity/persistence/no_thanks/entity_pool.rb
@@ -1,2 +1,24 @@
 module Relentity module Persistence::NoThanks::EntityPool
+
+  def add entity
+    entities[entity.id] = entity
+  end
+
+  def id id
+    entities[id]
+  end
+
+  def select &block
+    entities.values.select &block
+  end
+
+  def update entity
+  end
+
+  private
+
+  def entities
+    @entities ||= {}
+  end
+
 end end
--- spec/relentity/persistence/no_thanks/entity_pool_spec.rb
+++ spec/relentity/persistence/no_thanks/entity_pool_spec.rb
@@ -1,2 +1,54 @@
 module Relentity describe Persistence::NoThanks::EntityPool do
+
+  before :all do
+    module NoThanksPeople
+      extend Persistence::NoThanks::EntityPool
+    end
+    class NoThanksPerson
+      include Entity
+      entity_pool NoThanksPeople
+    end
+  end
+
+  describe '.add' do
+
+    it 'adds the passed object to the pool' do
+      object = Object.new
+      def object.id; :object; end
+      NoThanksPeople.id(:object).should == nil
+      NoThanksPeople.add object
+      NoThanksPeople.id(:object).should == object
+    end
+
+  end
+
+  describe '.id' do
+
+    it 'returns the Entity with the given id' do
+      NoThanksPeople.add rincewind = NoThanksPerson.new(id: :rincewind)
+      NoThanksPeople.add twoflower = NoThanksPerson.new(id: :twoflower)
+      NoThanksPeople.id(:rincewind).should == rincewind
+      NoThanksPeople.id(:twoflower).should == twoflower
+      NoThanksPeople.id(:auditor).should   == nil
+    end
+
+  end
+
+  describe '.select' do
+
+    it 'returns Entities fulfilling the block given' do
+      sandra = NoThanksPerson.new id: :sandra, given_names: ['Sandra'], surname: 'Battye'
+      NoThanksPeople.select { |p| p.id == :sandra }.should == [NoThanksPeople.id(:sandra)]
+    end
+
+  end
+
+  describe '.update' do
+
+    it 'exists and takes an argument' do
+      -> { NoThanksPeople.update Object.new }.should_not raise_error
+    end
+
+  end
+
 end end
