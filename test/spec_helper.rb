1871464850cf719f73e5e65578cc6b8d044c2962
Piotr Szotkowski <chastell@chastell.net>
2011-08-19 17:15:13 +0200

    port EntityRels spec to minitest/spec

--- test/spec_helper.rb
+++ test/spec_helper.rb
@@ -3,3 +3,20 @@ require 'minitest/autorun'
 require_relative '../lib/relentity'
 
 Relentity::EntityPool = Relentity::Persistence::NoThanks::EntityPool
+
+module People
+  extend Relentity::EntityPool
+end
+
+class Person
+  include Relentity::Entity
+  entity_pool People
+end
+
+sam   = Person.new id: :sam
+sybil = Person.new id: :sybil
+y_sam = Person.new id: :y_sam
+
+Relentity::Rel.new id: :duchy, refs: [sam, sybil],   rels: [:spouses, :spouses]
+Relentity::Rel.new id: :cow,   refs: [sam, y_sam],   rels: [:parents, :children]
+Relentity::Rel.new id: :'42',  refs: [y_sam, sybil], rels: [:children, :parents]
